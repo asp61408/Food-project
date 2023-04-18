d9c3458ef599f2d0ee220e24db32c3df63647ef2
Alex <39446110+asp61408@users.noreply.github.com>
2023-04-18 16:17:42 +0300

    71--changed html,css,js files

--- js/script.js
+++ js/script.js
@@ -124,18 +124,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-console.log(Date.getMonth())
-
-
-/*  */
 
 
 
 
+/* 71 Параметры документа, окна и работа с ними */
 
 
 
 
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
+
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+const width = box.scrollWidth;
+const height = box.scrollHeight;
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+console.log(width, height);
+
+btn.addEventListener('click', () => {
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+
+console.log(style.display);
+
+console.log(document.documentElement.scrollTop);
 
 
 
