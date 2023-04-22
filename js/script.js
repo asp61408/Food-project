5f6a9d2bea306e02d64dd87f5823f97fb2cda3dd
Alex <39446110+asp61408@users.noreply.github.com>
2023-04-22 13:00:18 +0300

    74--MutationObserver

--- js/script.js
+++ js/script.js
@@ -187,9 +187,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-/*  */
+/* 74 MutationObserver */
 
 
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box, {
+    childList: true
+})
+
+observer.disconnect();
 
 
 
