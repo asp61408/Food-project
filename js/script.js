daafbb52d9e7df2c3c338d10d1368b262ee6bd29
Alex <39446110+asp61408@users.noreply.github.com>
2023-04-27 09:10:06 +0300

    77--classes

--- js/script.js
+++ js/script.js
@@ -5,181 +5,237 @@
 
 
 
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
 
 
 
-//     /* 64 Tabs */
+    /* 64 Tabs */
 
 
-//     const tabs = document.querySelectorAll('.tabheader__item'),
-//           tabsContent = document.querySelectorAll('.tabcontent'),
-//           tabsParent = document.querySelector('.tabheader__items');
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-//     function hideTabContent() {
-//         tabsContent.forEach(item => {
-//             item.classList.add('hide');
-//             item.classList.remove('show', 'fade');
-//         });
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-//         tabs.forEach(item => {
-//             item.classList.remove('tabheader__item_active');
-//         });
-//     }
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
 
-//     function showTabContent(i = 0) {
-//             tabsContent[i].classList.add('show', 'fade');
-//             tabsContent[i].classList.remove('hide');
-//             tabs[i].classList.add('tabheader__item_active');
-//     }
+    function showTabContent(i = 0) {
+            tabsContent[i].classList.add('show', 'fade');
+            tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+    }
 
-//     hideTabContent();
-//     showTabContent();
+    hideTabContent();
+    showTabContent();
 
-//     tabsParent.addEventListener('click', (event) => {
-//         const target = event.target;
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
 
-//         if (target && target.classList.contains('tabheader__item')) {
-//             tabs.forEach((item, i) => {
-//                 if (target == item) {
-//                     hideTabContent();
-//                     showTabContent(i);
-//                 }
-//             })
-//         }
-//     });
-
-
-
-
-//     /* 69 Timer */
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
 
 
 
 
-//     const deadline = '2023-05-20';
+    /* 69 Timer */
+
+
+
+
+    const deadline = '2023-05-20';
     
-//     function getTimeRemaining(endtime) {
-//         let days, hours, minutes, seconds;
-//         const t = Date.parse(endtime) - Date.parse(new Date());
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
 
-//         if (t <= 0) {
-//             days = 0;
-//             hours = 0;
-//             minutes = 0;
-//             seconds = 0;
-//         } else {
-//             days = Math.floor((t / (1000 * 60 * 60 * 24))),
-//             hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-//             minutes = Math.floor((t / 1000 / 60) % 60),
-//             seconds = Math.floor((t / 1000) % 60);
-//         }
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
 
-//         return {
-//             'total': t,
-//             days,
-//             hours,
-//             minutes,
-//             seconds
-//         };
-//     }
+        return {
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
 
-//     function getZero(num) {
-//         if (num >= 0 && num < 10) {
-//             return `0${num}`;
-//         } else {
-//             return num;
-//         }
-//     }
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
 
-//     function setClock(selector, endtime) {
-//         const timer = document.querySelector(selector),
-//               days = timer.querySelector('#days'),
-//               hours = timer.querySelector('#hours'),
-//               minutes = timer.querySelector('#minutes'),
-//               seconds = timer.querySelector('#seconds'),
-//               timeInterval = setInterval(updateClock, 1000);
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
 
-//         updateClock();
+        updateClock();
 
-//         function updateClock() {
-//             const t = getTimeRemaining(endtime);
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
-//             days.innerHTML = getZero(t.days);
-//             hours.innerHTML = getZero(t.hours);
-//             minutes.innerHTML = getZero(t.minutes);
-//             seconds.innerHTML = getZero(t.seconds);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
-//             if (timeInterval <= 0) {
-//                 clearInterval(timeInterval);
-//             }
-//         }
+            if (timeInterval <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
 
-//     }
+    }
     
-//     setClock('.timer', deadline);
+    setClock('.timer', deadline);
 
 
 
-//     /* 72, 73 Modal */
+    /* 72, 73 Modal */
    
 
-//     const modalTrigger = document.querySelectorAll('[data-modal]'),
-//           modalCloseBtn = document.querySelector('[data-close]'),
-//           modal = document.querySelector('.modal');
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modalCloseBtn = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
 
-//     function openModal() {
-//         modal.classList.add('show');
-//         modal.classList.remove('hide');
-//         document.body.style.overflow = 'hidden';
-//         clearInterval(modalTimerID);
-//         window.removeEventListener('scroll', showModalByScroll);
-//     }
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerID);
+        window.removeEventListener('scroll', showModalByScroll);
+    }
 
-//     modalTrigger.forEach(btn => {
-//         btn.addEventListener('click', openModal);
-//     });
-
-
-//     function closeModal() {
-//         modal.classList.add('hide');
-//         modal.classList.remove('show');
-//         document.body.style.overflow = '';
-//     }
-
-//     modalCloseBtn.addEventListener('click', closeModal);
-
-//     modal.addEventListener('click', e => {
-//         if (e.target === modal) {
-//             closeModal();
-//         }
-//     });
-
-//     document.addEventListener('keydown', (e) => {
-//         if (e.code === 'Escape' && modal.classList.contains('show')) {
-//             closeModal();
-//         }
-//     });
-
-//     const modalTimerID = setTimeout(openModal, 15000);
-
-//     function showModalByScroll() {
-//         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-//             openModal();
-//             // window.removeEventListener('scroll', showModalByScroll);
-//         }
-//     }
-
-//     window.addEventListener('scroll', showModalByScroll);
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
 
 
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
 
+    modalCloseBtn.addEventListener('click', closeModal);
 
-//     /*  */
+    modal.addEventListener('click', e => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerID = setTimeout(openModal, 15000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            // window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 
 
 
-// });
+
+    /*  */
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/* 77 Classes */
+
+
+
+
+
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColorRectangleWithText extends Rectangle {
+    constructor(height, width, text, colorBg) {
+        super(height, width);
+        this.text = text;
+        this.colorBg = colorBg;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.colorBg}`);
+    }
+}
+
+const div = new ColorRectangleWithText(25, 10, 'Hello World', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+
+const sqare = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(sqare.calcArea());
+console.log(long.calcArea());
+
+
+
 
 
 
